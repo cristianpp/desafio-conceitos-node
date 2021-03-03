59eef9a18d9db003e2ad46e3d43e4c818514b3c8
cristian Pietro <cristian.pietro@gmail.com>
2021-03-02 21:47:47 -0300

    Implementacao de novas rotas

--- src/app.js
+++ src/app.js
@@ -32,7 +32,7 @@ app.get("/repositories", (request, response) => {
 app.post("/repositories", (request, response) => {
    const {title, url, techs} = request.body;
 
-   const repository = {id: uuid(), title, url, techs, likes: 10}
+   const repository = {id: uuid(), title, url, techs, likes: 0}
 
    repositories.push(repository);
 
@@ -44,7 +44,7 @@ app.put("/repositories/:id", (request, response) => {
     const { id } = request.params;
     const { title, url, techs } = request.body;
 
-    const repositoryIndex = repositories.findIndex(repository => repository.id = id);
+    const repositoryIndex = repositories.findIndex(repository => repository.id == id);
 
     if (repositoryIndex < 0 ){
       return response.status(400).json( { error: 'Repository not found' });
@@ -68,11 +68,45 @@ app.put("/repositories/:id", (request, response) => {
 });
 
 app.delete("/repositories/:id", (request, response) => {
-  // TODO
+  const { id } = request.params;
+
+  const repositoryIndex = repositories.findIndex(repository => repository.id == id);
+
+  if (repositoryIndex < 0){
+    return response.status(400).json({ error: 'Repository not found'});
+  }
+
+  repositories.splice(repositoryIndex, 1);
+
+  return response.status(204).send();
+
 });
 
 app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+   const { id } = request.params;
+
+   const repositoryIndex = repositories.findIndex(repository => repository.id == id);
+
+   if (repositoryIndex < 0 ){
+     return response.status(400).json({ error: 'Repository not found'});
+   }
+
+   const loadRepository = repositories[repositoryIndex];
+   const moreLikes = loadRepository.likes + 1;
+
+   const repository = {
+     id,
+     title: loadRepository.title,
+     url: loadRepository.url,
+     techs: loadRepository.techs,
+     likes: moreLikes,
+   }
+
+   repositories[repositoryIndex] = repository;
+
+   return response.json(repository);
+
+
 });
 
 module.exports = app;
